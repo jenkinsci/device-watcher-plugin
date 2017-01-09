849f38a11a8c70e6218fb7447f91d18ca625f12f
Paul Buckley <pbuckley4192@gmail.com>
2017-01-09 14:37:59 +0000

    Moved scan code to a TimerTask. Updated the monitor text. Used a more capable isHostReachable (no port needed)

--- src/main/java/com/sonicwind/DeviceWatch/DeviceMonitor.java
+++ src/main/java/com/sonicwind/DeviceWatch/DeviceMonitor.java
@@ -1,8 +1,12 @@
 package com.sonicwind.DeviceWatch;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.Socket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import hudson.Extension;
 import hudson.model.AdministrativeMonitor;
@@ -10,6 +14,9 @@ import hudson.model.AdministrativeMonitor;
 @Extension
 public class DeviceMonitor extends AdministrativeMonitor
 {
+   ArrayList<String> offline = new ArrayList<>();
+
+   Timer scanTimer = null;
 
    @Override
    public String getDisplayName()
@@ -20,46 +27,97 @@ public class DeviceMonitor extends AdministrativeMonitor
    @Override
    public boolean isActivated()
    {
-      DeviceGlobalConfiguration ds = new DeviceGlobalConfiguration().get();
 
-      if ((ds == null || ds.getAddresses() == null) || !isEnabled())
+      if (!isEnabled())
       {
          return false;
       }
 
-      boolean reachable = false;
-      for (String s : ds.getAddresses().split(","))
+      if (scanTimer == null)
       {
-         s = s.trim();
-         System.out.println(s);
-
-         try
-         {
-            try (Socket soc = new Socket())
-            {
-               soc.connect(new InetSocketAddress(s, 9000), 1000);
-            }
-            reachable = true;
-         }
-         catch (IOException ex)
-         {
-            reachable = false;
-         }
-
-         System.out.println(s + " is " + reachable);
-
-
+         scanTimer = new Timer(true);
+         scanTimer.scheduleAtFixedRate(new ScanTimer(), 0, 10000);
       }
 
-      return !reachable;
+      return !offline.isEmpty();
+   }
+
+   public ArrayList<String> getListOffline()
+   {
+      return offline;
+   }
+
+   private class ScanTimer extends TimerTask
+   {
+
+      @Override
+      public void run()
+      {
+         ArrayList<String> offlineDevices = new ArrayList<>();
+         LinkedHashSet<String> device = new LinkedHashSet<>();
+         DeviceGlobalConfiguration ds = new DeviceGlobalConfiguration().get();
+
+         if (ds == null || ds.getAddresses() == null)
+         {
+            return;
+         }
+
+         String unfiltered[] = ds.getAddresses().split(",");
+
+         for (String unfilteredDevice : unfiltered)
+         {
+
+            unfilteredDevice = unfilteredDevice.trim();
+            if (unfilteredDevice.isEmpty())
+            {
+               continue;
+            }
+            device.add(unfilteredDevice);
+
+         }
+
+         offlineDevices.clear();
+         for (String s : device)
+         {
+            System.out.println("Checking: " + s);
+
+            try
+            {
+               InetAddress host = InetAddress.getByName(s);
+
+
+               if (!host.isReachable(10000))
+               {
+                  System.out.println(s + "was unreachable");
+                  offlineDevices.add(s);
+               }
+            }
+            catch (UnknownHostException ue)
+            {
+               ue.printStackTrace();
+            }
+            catch (IOException ex)
+            {
+               ex.printStackTrace();
+               System.out.println(s + " has an IO Exception");
+               if (!offline.contains(s))
+               {
+                  offlineDevices.add(s);
+               }
+            }
+
+
+         }
+
+         offline = offlineDevices;
+      }
    }
 
 }
 
 /*
- * Copyright 2016 Pilz Ireland Industrial Automation Ltd.
- * All Rights Reserved. PILZ PROPRIETARY/CONFIDENTIAL.
- * Use is subject to license terms.
+ * Copyright 2016 Pilz Ireland Industrial Automation Ltd. All Rights Reserved. PILZ
+ * PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  * 
  * Created on 13 Dec 2016 by pbuckley
  */
